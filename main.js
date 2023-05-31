8003a6586f8fc4cde500e9f28041a0773c7d3344
Sher06777 <132756489+Sher06777@users.noreply.github.com>
2023-05-31 18:22:58 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-let input1 = document.getElementById('input1')
-let input2 = document.getElementById('input2')
-let buttn = document.getElementById('buttn')
-let value1 = input1.value
-
-buttn.onclick = () =>{
-    input1.value = input2.value
-    input2.value = value1
-}
-
-
-// const buttn = document.querySelector('.buttn')
+// let input1 = document.getElementById('input1')
+// let input2 = document.getElementById('input2')
+// let buttn = document.getElementById('buttn')
+// let value1 = input1.value
 
 // buttn.onclick = () =>{
-//     let a = document.querySelector('.input1').value
-//     document.querySelector('.input1').value = document.querySelector('.input2').value
-//     document.querySelector('.input2').value = a
+//     input1.value = input2.value
+//     input2.value = value1
 // }
+
+
+
+const buttn = document.querySelector('#buttn')
+
+buttn.onclick = () =>{
+    let a = document.querySelector('#input1').value
+    document.querySelector('#input1').value = document.querySelector('#input2').value
+    document.querySelector('#input2').value = a
+}
 
